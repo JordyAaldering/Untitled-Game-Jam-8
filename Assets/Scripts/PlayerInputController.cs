206b3ef6208d4cddedd72f3ffb0dba4093782685
Jordy Aaldering <aalderingjordy@gmail.com>
2019-10-11 16:46:26 +0200

    Initial commit

---
+++ Assets/Scripts/PlayerInputController.cs
@@ -0,0 +1,20 @@
+﻿using UnityEngine;
+
+public class PlayerInputController : MonoBehaviour
+{
+    private CharacterController2D controller;
+
+    private void Start()
+    {
+        controller = GetComponent<CharacterController2D>();
+    }
+
+    private void Update()
+    {
+        float move = Input.GetAxis("Horizontal");
+        bool crouch = Input.GetKeyDown(KeyCode.LeftControl);
+        bool jump = Input.GetKeyDown(KeyCode.Space);
+        
+        controller.Move(move, crouch, jump);
+    }
+}
---
+++ README.md
@@ -0,0 +1,11 @@
+# Untitled Game Jam 8
+Theme: 'Death'
+
+# Built With
+* [Unity](https://unity.com/)
+* [Rider](https://www.jetbrains.com/rider/)
+* [Krita](https://krita.org/en/)
+
+# Acknowledgements
+* [Character Controller](https://github.com/Brackeys/2D-Character-Controller)
+* [Graveyard Tileset](https://opengameart.org/content/free-graveyard-platformer-tileset)
