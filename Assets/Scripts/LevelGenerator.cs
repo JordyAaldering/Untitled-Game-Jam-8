8ddab9994e8a68f85879ee42e80d7ab34a6186d1
Jordy Aaldering <aalderingjordy@gmail.com>
2019-10-11 17:49:48 +0200

    Basic Level Gen

---
+++ Assets/Scripts/LevelGenerator.cs
@@ -0,0 +1,24 @@
+﻿using Extensions;
+using UnityEngine;
+
+public class LevelGenerator : MonoBehaviour
+{
+    [SerializeField] private int levelSize = 8;
+
+    private void Start()
+    {
+        GenerateLevel();
+    }
+
+    private void GenerateLevel()
+    {
+        for (int i = 0; i < levelSize; i++)
+        {
+            Instantiate(
+                PieceManager.instance.pieces.GetRandom(),
+                new Vector3(i * 8f, 0f, 0f),
+                Quaternion.identity
+            );
+        }
+    }
+}
---
+++ Assets/Scripts/PieceManager.cs
@@ -0,0 +1,21 @@
+﻿using UnityEngine;
+
+public class PieceManager : MonoBehaviour
+{
+    public static PieceManager instance;
+
+    private void Awake()
+    {
+        if (instance == null)
+        {
+            instance = this;
+        }
+        else if (instance != this)
+        {
+            Debug.LogError("Instance already exists.");
+            Destroy(gameObject);
+        }
+    }
+
+    public GameObject[] pieces;
+}
